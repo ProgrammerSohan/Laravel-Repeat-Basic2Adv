3b2736070016a9492bdf787fbdc1966d20c655a1
ProgrammerSohan <sohanofficial2022@gmail.com>
2025-06-09 23:40:29 +0600

    04.3-9s-more validation rules

--- app/Http/Controllers/loginController.php
+++ app/Http/Controllers/loginController.php
@@ -18,12 +18,32 @@ class loginController extends Controller
 
                 //dd($request);
                // dd($request->all());
+               /*
                 $request->validate([
                     'name'=> 'required',
                     'email'=> 'required',
                     'password'=>'required'
 
                 ]);
+
+               return $request;*/
+               /*
+               $request->validate([
+                    'name' => 'required|alpha',
+                    'email'=> 'required',
+                    'password'=>'required'
+
+
+               ]);
+
+               return $request;*/
+
+               $request->validate([
+                    'name'=> ['required','alpha','min:6','max:10'],
+                    'email'=> ['required','email'],
+                    'password'=>'required'
+
+               ]);
 
                return $request;
 
